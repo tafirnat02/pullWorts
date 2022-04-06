293a106c757fb6237ba4bd1e1f4abf720d59c97b
tafirnat02 <103073188+tafirnat02@users.noreply.github.com>
2022-04-06 14:11:53 +0200

    new test get function

--- app1.js
+++ app1.js
@@ -1,18 +1,61 @@
 "use strict";
 //kelime bilgilerini tutacak js objesi githubdan jsdelivr araciligyla asekron fetch yapisi kullanilarak import edilir.
-var newWort =""
-const url = "https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/wortsObj.json"
-const loadScript = async (url) => {
-    try {
-        const response = await fetch(url)
-        const json = await response.text()
-        newWort = JSON.parse(json);
-      } catch (error) {
-        console.log(error)
-      }
-    }
+var newWort = "";
+var wortList = "";
 
-loadScript(scriptUrl).then(()=>{
-    console.log(newWort)  
-})
-//kelimeler 
+const wortListUrl =
+  "https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/wortList.json";
+const objUrl =
+  "https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/wortsObj.json";
+
+const loadScript = async (url) => {
+  try {
+    const response = await fetch(url);
+    const json = await response.text();
+    return JSON.parse(json);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+loadScript(objUrl)
+  .then((res) => {
+    newWort = res;
+  })
+  .then(() => {
+    loadScript(wortListUrl)
+      .then((res) => {
+        wortList = res;
+      })
+      .then(() => {
+        console.log(newWort);
+        console.log(wortList);
+      });
+  });
+//kelime cejimleri...
+var wort = "";
+var wortUrl = "https://www.verbformen.de/?w=haben";
+const loadDoc = async (url) => {
+  await fetch(url, { mode: "no-cors" })
+    .then(function (response) {
+      console.log(response);
+      console.log("durum: " + response.status);
+      return response.text();
+    })
+    .then(function (html) {
+      var parser = new DOMParser();
+      console.log("---");
+
+      return parser.parseFromString(html, "text/html");
+    })
+    .catch(function (err) {
+      // There was an error
+      console.warn("Something went wrong.", err);
+    });
+};
+
+loadDoc(wortUrl).then((doc) => {
+  //console.log(doc)
+  console.log(doc.querySelector("body"));
+  // getWort()
+});
