921262768ec1274ad191bdb51196f8c8568f6b2a
tafirnat02 <103073188+tafirnat02@users.noreply.github.com>
2022-04-06 06:07:09 +0200

    kelime listesi

--- app1.js
+++ app1.js
@@ -1,16 +1,18 @@
 "use strict";
-const rpRegExp = /⁰|¹|²|³|⁴|⁵|⁶|⁷|⁸|⁹|\n/g;
-const brExp = /·/g;
-const empty = "";
-
-//kelime bilgilerini tutacak js objesi import edilir.
+//kelime bilgilerini tutacak js objesi githubdan jsdelivr araciligyla asekron fetch yapisi kullanilarak import edilir.
 var newWort =""
-fetch("https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/wortsObj.json")
-   .then(res => res.text())
-   .then((res) => {
-       const obj = JSON.parse(res);
-       console.log(obj);
-   })
-   .catch((err) => {
-    console.log(err)
-  });
+const url = "https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/wortsObj.json"
+const loadScript = async (url) => {
+    try {
+        const response = await fetch(url)
+        const json = await response.text()
+        newWort = JSON.parse(json);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+loadScript(scriptUrl).then(()=>{
+    console.log(newWort)  
+})
+//kelimeler 
