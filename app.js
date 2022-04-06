6a6c51f3401600c6b407503cef3cbfd1bb319460
tafirnat02 <103073188+tafirnat02@users.noreply.github.com>
2022-04-06 05:15:44 +0200

    app import js obje new wort

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
+"use strict";
 const rpRegExp = /⁰|¹|²|³|⁴|⁵|⁶|⁷|⁸|⁹|\n/g;
 const brExp = /·/g;
 const empty = "";
 
+//kelime bilgilerini tutacak js objesi import edilir.
+var newWort =""
+fetch("https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/wortsObj.json")
+   .then(res => res.text())
+   .then((res) => {
+       const obj = JSON.parse(res);
+       console.log(obj);
+   })
+   .catch((err) => {
+    console.log(err)
+  });
