afb11b602ae7b80eb9e2d29d083e558ddc99da04
tafirnat02 <103073188+tafirnat02@users.noreply.github.com>
2022-04-07 01:59:08 +0200

    retun ile newwort alinip ön kodda islenecek

--- app1.js
+++ app1.js
@@ -1,3 +1,99 @@
+/****************
+ * Öncelikle sayfaya 
+ *    -calisacak scripti ve 
+ *    -kelime listesini 
+ * dahil ederiz....
+ */
+
+//getWort scrpti yüklenir...
+const getWrt="https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/getWort_03.js";
+const wortListUrl="https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/wortList.json";
+var newWortArr = []
+let oldScript = document.querySelector("head").lastChild  
+
+checkStO()
+
+let script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = getWrt;
+
+if(oldScript.src !== getWrt){
+document.head.appendChild(script);
+}
+
+//fetch ile urllerdeki jsObje ve kelime arrayi getirilir
+const loadScript = async (url) => {
+  try {
+    const response = await fetch(url);
+    const json = await response.text();
+    return JSON.parse(json)
+  } catch (error) {
+    console.log("Something went wrong.", error);
+  }
+};
+
+loadScript(wortListUrl)
+      .then((res) => {//kelime listesi alinip döngüye sokulur
+        newWortArr = [ ...res ]  
+        return res 
+    })
+
+//durum check edilir
+function checkStO(){
+    setTimeout(()=>{
+        console.log(document.querySelector("head").lastChild.src)
+        console.log(newWortArr)
+    }, 300);
+}
+
+/****************************************************
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * ************************************************* */
+
+
+
+
+
+
+
+ newWortArr.forEach(function(wort) {
+  promises.push(
+    
+         var loadDoc = async (url) => {
+             await fetch(url, { mode: "no-cors" })
+             .then(function (response) {
+                 console.log(response);
+                 console.log("durum: " + response.status);
+                 return response.text();
+             })
+             .then(function (html) {
+                 var parser = new DOMParser();
+                 doc= parser.parseFromString(html, "text/html");
+                 getWort()
+             })
+             .catch(function (err) {
+                 console.warn("Something went wrong.", err);
+             });
+         };
+
+         loadDoc(`https://www.verbformen.de/?w=${wort}`)
+  )
+})
+
+
+
+
+
+
+
+
+
+
 "use strict";
 //kelime bilgilerini tutacak js objesi githubdan jsdelivr araciligyla asekron fetch yapisi kullanilarak import edilir.
 var newWort = "";
