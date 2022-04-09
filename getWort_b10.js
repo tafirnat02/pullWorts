083a0201e6519df84e57759d3b28d7c8c3a3ca19
tafirnat02 <103073188+tafirnat02@users.noreply.github.com>
2022-04-09 14:04:10 +0200

    ddge

--- getWort_b10.js
+++ getWort_b10.js
@@ -270,8 +270,8 @@ function setStatus(ele, verb) {
 }
 /**** kelimenin TR karsiligi alinir */
 function getLang(){
-  let srcL1 = document.querySelector('span[lang="tr"]')
-  let srcL2 = document.querySelector('dd[lang="tr"]')
+  let srcL1 = doc.querySelector('span[lang="tr"]')
+  let srcL2 = doc.querySelector('dd[lang="tr"]')
   if(checkEl(srcL1)){
        wort_Obj.lang_TR = srcL1.innerText
   }else if(checkEl(srcL2)){
