e5b062b102d18dcba89794518d11d46fb89a819f
tafirnat02 <103073188+tafirnat02@users.noreply.github.com>
2022-04-10 00:56:23 +0200

    ghfrgg

--- getWort_d00.js
+++ getWort_d00.js
@@ -303,7 +303,7 @@ function getLang(callback, wrt){
               callback(wrt)
           })
           .catch(err => {
-            console.log("%Google Translate API Hatasi\ncKelime: " +  newWort.wrt.wort ,"color: yellow;  font-size: 13px");
+            console.log("%cGoogle Translate API Hatasi\ncKelime: " +  newWort.wrt.wort ,"color: yellow;  font-size: 13px");
               //console.log("Google Translate API Hatasi:\n"+  err)
               callback(wrt)
           });
