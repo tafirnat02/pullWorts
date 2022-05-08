b4d5b20a2a19ba97dadb367db8bf34def7dc8807
tafirnat02 <103073188+tafirnat02@users.noreply.github.com>
2022-05-09 00:25:51 +0200

    fgfg

--- wortApp/_getWort_multiple_a08.js
+++ wortApp/_getWort_multiple_a08.js
@@ -1,9 +1,9 @@
 
 //ilgili urldeki js kodu sayfanin head kismina eklenir....
 let script = document.createElement('script');
-script.type = 'text/javascript';
-//script.type = 'module';
-script.src = "https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/wortApp/_getWort_multiple_a07.js";
+//script.type = 'text/javascript';
+script.type = 'module';
+script.src = "https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/wortApp/_getWort_multiple_a08.js";
 //head taginin en sonuna yerlestirilir
 document.head.appendChild(script);
 //belirli bir ögeden hemen sonra bulunmasi istenilirse:
