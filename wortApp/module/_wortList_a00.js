69b95dd3f9f44ac09c9d3a7d8a7ed5d65dffec83
tafirnat02 <103073188+tafirnat02@users.noreply.github.com>
2022-05-09 02:15:22 +0200

    fdgdg

---
+++ wortApp/module/_wortList_a00.js
@@ -0,0 +1,37 @@
+/*
+Modul Baglami: Kelime listasini dahil eder... 
+*/
+
+/*-------- Modul Disina Aktarilanlar ---------*/
+export const getWortList=(newList="")=>{
+    console.log("🚩 running... ≣⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮⋮");
+    if(!!newList) return //eger kelime listesi elden girilmis ise dosyadan kelime alinmaz
+    loadList()
+    //kelimeler dosyadan array olarak sayfaya dahil edilir...
+ return wortList
+}
+
+/*-------- Modul Icerigindeki Islemler ---------*/
+const url='https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/oldFiles/wortlist.json'
+const mainWortList =[]
+
+function loadList () {
+    //resolve(loadWort());//promise ile sirali olarak js filler eklenir...
+    fetch(url)
+      .then((response) => {
+        return response.text();
+      }) // or .json()
+      .then((data) => {
+        return JSON.parse(data);
+      })
+      .then((list) => {
+        wortList.push(...list);
+      })
+      .catch((err) => {
+        console.clear()
+        console.log(
+          `Kelime listesi alinirken hata oldu. Kelime urlini kontrol edin! (m:wortList, f:loadApp) ${url}`
+        );
+        console.log( err)
+      });
+};
