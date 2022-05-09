59ff328e5d7f698e9c2839dbb930f1e6a0513e84
tafirnat02 <103073188+tafirnat02@users.noreply.github.com>
2022-05-09 05:58:58 +0200

    fgdfgd

--- getModuls_a31.js
+++ getModuls_a31.js
@@ -4,7 +4,7 @@
  *
  */
 const url_importModuls =
-  "https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/wortApp/_importModuls_a23.js";
+  "https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/wortApp/_importModuls_a24.js";
 
 //dosya konumu kontrol edilip, moduller sayfaya dahil edilir...  
 try {
