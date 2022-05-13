9165066a4ab26f4508f81f5b45ebf41274230bb2
tafirnat02 <103073188+tafirnat02@users.noreply.github.com>
2022-05-14 00:37:18 +0200

    fdgfdg

--- _getModuls_b00.js
+++ _getModuls_b00.js
@@ -11,7 +11,7 @@
 
 const urlChecker = { url: undefined }, //dosya konumu kontrol edilip, moduller sayfaya dahil edilir...
   url_importModuls =
-    "https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/wortApp/_importModuls_b10.js";
+    "https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/wortApp/_importModuls_b11.js";
     
 if (!checkFile(url_importModuls, "m:getModuls, f:importModuls"))
   () => {
