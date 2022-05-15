d2ffc360561c75461dc4ca22f6033a847eff3703
tafirnat02 <103073188+tafirnat02@users.noreply.github.com>
2022-05-15 21:23:03 +0200

    ewew

--- wortApp/_importModuls_c34.js
+++ wortApp/_importModuls_c34.js
@@ -6,28 +6,29 @@ Kullanilacak tüm ögeler ilgili modul icinden export islemi sonrasi burada impo
 
 //import {myFunc} from "./module/_img_a00" //image islemlerini yapar
 //import {myFunc} from "./module/_lang_a00" //dil islemlerini yapar
-import { getWortList } from "./module/_wortList_b16.js"; //kullanilacak kelimleri alir
+//import { getWortList } from "./module/_wortList_b16.js"; //kullanilacak kelimleri alir
 import { baseFun } from "./module/_zBase_b06.js"; //bu bir dizin altindaki tüm ögleri 'base' adli degiskene export eder...
-import { getDoc } from "./module/_documents_a06.js"; //document/HTML dizin olarak ham verileri tutar
+//import { getDoc } from "./module/_documents_a06.js"; //document/HTML dizin olarak ham verileri tutar
 //import {getWortObject} from "./module/_getWortObj_a03"; //HML  olarak alinan dizin ögelerini nesne olusturmaya yönlendirir
 //import {newWortObject, testASCVBG} from "./module/_creatWortObj_a02" //HTML'den wort nesnesinin icerigini toplar
 //import sonrasi ilgili ögeler yürütülür...
 
 
-const _base = ()=>{
-   const _wortList = ()=>{
-     const _HTMLdocs = () =>{
-       const test=()=>{
-         console.log('test ---')
-        }
-        getDoc(()=>test.call());
-      }
-      getWortList(()=>_HTMLdocs())
-  }
-  baseFun(()=>_wortList())
+
+const loadBase = ()=>{
+  return new Promise((resolve,reject)=>{
+  baseFun()
+  setTimeout(() => {
+    resolve()
+  }, 100);
+})
 }
 
-_base.call()
+
+loadBase
+.then(
+  console.log(item)
+)
 
 
 /*
