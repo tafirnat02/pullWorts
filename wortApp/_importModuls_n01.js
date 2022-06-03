b9580d6cabb71209d3ab66de4de2303a4f0629eb
tafirnat02 <103073188+tafirnat02@users.noreply.github.com>
2022-06-04 01:00:58 +0200

    dsdsf

--- wortApp/_importModuls_n01.js
+++ wortApp/_importModuls_n01.js
@@ -14,7 +14,7 @@ clone array localde tutulacak....
 /**  --- import edilen ögeler --- */
 import { runApp } from "./module/_creatWortObj_e07.js"; //HTML'den wort nesnesinin icerigini toplar
 import { getDoc } from "./module/_documents_b05.js"; //document/HTML dizin olarak ham verileri tutar
-import { getWortObject } from "./module/_getWortObj_b03.js"; //HTML  olarak alinan dizin ögelerini nesne olusturmaya yönlendirir
+import { getWortObject } from "./module/_getWortObj_b04.js"; //HTML  olarak alinan dizin ögelerini nesne olusturmaya yönlendirir
 import {getImg} from "./module/_img_b03.js" //image islemlerini yapar
 import { getLang } from "./module/_lang_b01.js"; //dil islemlerini yapar
 import { getWorteList } from "./module/_wortList_c010.js"; //kullanilacak kelimleri alir
