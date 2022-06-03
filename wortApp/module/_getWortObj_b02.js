d20ea2e07d6fb1f911ddcba46433654e6bb80b31
tafirnat02 <103073188+tafirnat02@users.noreply.github.com>
2022-06-04 00:48:17 +0200

    dsfsf

--- wortApp/module/_getWortObj_b02.js
+++ wortApp/module/_getWortObj_b02.js
@@ -21,6 +21,7 @@ const setDoc = async (callback) => {
 };
 //HTMLdocs dizinindeki tüm ögeler icin setDoc ile islem yapilir
 const docs = async (callback) => {
+  console.log(`getWortObj--  len:${len} index:${index}`)
   if (len >= index){
     byController.worts = true; //item.search() ile bu asamnin tamamlandigini teyit icin controlObj'de worts propertysi olusturulur...
     return true
