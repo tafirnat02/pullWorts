eee6e44ff90f5f813040a0704d2a1023fc9a1168
tafirnat02 <103073188+tafirnat02@users.noreply.github.com>
2022-06-06 05:05:07 +0200

    fdf

--- appStarter.js
+++ appStarter.js
@@ -1,75 +1,27 @@
-/**
- * Buradaki kod console yapistirilip run edilerek baslatilir.
- * Bu kod ile moduller yüklenir sayfaya ve esrisilir hale getirilir...
- * Asil islem yapilacak kodlar burada dahil edilen js dosyasindadir...
- *
- */
+//Yeni aranacak kelimelerin arasina virgül konulmali
+var neueAbfrage="";// ör:"Gemüse,Fenster,Tüte"
 
+//modul import js dosyasi
+const url_moduls = "https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/wortApp/_importModuls_n17.js"
+//modul dosya durumu kontrol edilir
 
-/*
+ //sayfaya modulleri import eder
+ function loadModuls(){return new Promise((resolve)=>{let head_ =
+   document.querySelector("head"),scrpt = document.createElement("script"); 
+   scrpt.type = "module";scrpt.src = url_moduls;document.head.appendChild(scrpt);
+ head_.insertBefore(scrpt, head_.lastChild);resolve()});}
 
-server hatasinda kalinan sayiyi storage e aktararak oradan baslatilabilir.....
+function existFile(url){
+if(typeof runApp !== 'undefined') runApp()
+var http = new XMLHttpRequest();
+http.open('HEAD', url, false);http.send();
+ if(http.status===200) return true
+     let txt_404 = ['warn',`Dosya konumu hatali! Url'yi kontrol edin.\n${url}\n`],
+      txt_e=['error',`Hata meydana geldi!\n`],
+      txt = http.status===404?txt_404:txt_e;
+      window.console[txt[0]](txt[1],err) 
+    return false
+ }
 
- */
+ if (existFile(url_moduls)) loadModuls.call()
 
-const urlChecker = { url: undefined };
-//ilgili urldeki js kodu sayfanin head kismina eklenir....
-const url_getModuls ="https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/getModuls_a65.js";
-
-if (checkFile(url_getModuls)) {
-  let script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = url_getModuls;
-  //head taginin en sonuna yerlestirilir
-  document.head.appendChild(script);
-  //belirli bir ögeden hemen sonra bulunmasi istenilirse:
-  let head = document.querySelector("head");
-  head.insertBefore(script, head.firstChild);
-}
-
-/* --- cdn dosya yolunun gecerli olup olmadigini kontrol eder --- */
-//islem sonrasi kontrol icin sonuc url eslestirilerek bu nesneden check edilir.
-async function checkFile(url, pos = "") {
-  pos=!!pos?pos:'m:appStarter, f:checkFile)';
-  urlChecker.url = false; //obje degeri default hale getirilir...
-  await fetch(url)
-    .then((response) => {
-      if (response.status === 404) throw 404;
-      urlChecker.url = true; //url erisilebilir...
-    })
-    .catch((err) => {
-      let txt404 = ['warn',`Dosya konumu hatali! url'yi kontrol edin. ${pos}\n${url}`],
-          txte=['error',`Hata meydana geldi! ${pos}`],
-          txt = err===404?txt404:txte;
-      window.console[txt[0]](txt[1],'(m:appStarter, f:checkFile)',err)
-
-      /*
-      if (err === 404) {
-        console.warn(
-          `Dosya konumu hatali! url'yi kontrol edin ${pos}.\n${url}`,
-          err
-        );
-      } else {
-        console.error("Hata meydana geldi! (m:appStarter, f:checkFile)", err);
-      }
-      */
-    });
-}
-
-
-/*
-Dizin Yapisi:
-📂
-  |_📇appStarter.js         .../appStarter.js"📍
-  |_📇getMosuls_*.js        .../getMosuls_*.js"  🟡
-  |_📂WortApp               .../wortApp
-    |_📇_importModuls_*.js  .../wortApp/_importModuls_*.js  
-    |_📂module                  ./module
-    |_📇_Documents_*.js         ./module/_Documents_*.js 
-    |_📇_img_*.js               ./module/_img_*.js 
-    |_📇_lang_*.js              ./module/_lang_*.js
-    |_📇_wortList_*.js          ./module/_wortList_*.js   
-    |_📇_wortObj_*.js           ./module/_wortObj_*.js
-    |_📇_zBase_*.js             ./module/_zBase_*.js
-    |_📇wortList.json           ./module/wortList.json    
-*/
---
+++ wortApp/_importModuls_n18.js
@@ -0,0 +1,180 @@
+/**
+Kullanilacak tüm ögeler ilgili modul icinden export islemi sonrasi burada import edilir....
+
+son kontrol:
+genetiv kismini tam olarak alinmali....
+
+*/
+
+/**  --- import edilen ögeler --- */
+import { runApp } from "./module/_creatWortObj_e11.js"; //HTML'den wort nesnesinin icerigini toplar
+import { getDoc } from "./module/_documents_b11.js"; //document/HTML dizin olarak ham verileri tutar
+import { getWortObject } from "./module/_getWortObj_b08.js"; //HTML  olarak alinan dizin ögelerini nesne olusturmaya yönlendirir
+import { getImg } from "./module/_img_b03.js"; //image islemlerini yapar
+import { getLang } from "./module/_lang_b01.js"; //dil islemlerini yapar
+import { getWorteList } from "./module/_wortList_c010.js"; //kullanilacak kelimleri alir
+import { baseFun } from "./module/_zBase_c17.js"; //bu bir dizin altindaki tüm ögleri 'base' adli degiskene export eder...
+//import sonrasi ilgili ögeler yürütülür...
+
+let maxTime = 1500;
+
+async function appRun() {
+  console.clear();
+  if (typeof window.appRun === "undefined") window.appRun = appRun;
+
+  await controller()
+    .then((result) => {
+      if (result) {
+        let tryAgain = window.confirm(
+          "Girilen kelimeler icin islem yapildi!\nIslem tekrarlansin mi?"
+        );
+        if (!tryAgain)
+          return console.warn(
+            "Kelimeler icin islem tekrarlanmasi iptal edildi.\n",
+            worteList
+          );
+        return finish(); //son alinan wortObj tekrar ekrana basilir...
+      }
+      console.log("liste degisti, yeniden cagrilacak...");
+      console.log("🚩running... ▱▱▱▱▱▱▱▱▱▱▱ 0%"); //baslama bildirimi...
+      storage.remove("lastWortList");
+      getHTMLdoc();
+    })
+    .catch((error) => {
+      if (error === "notWort") {
+        console.warn(
+          "Islem yapilacak kelime bulunamadi!\n'neueAbfrage' ile yeni kelime girisi yapin!"
+        );
+      } else {
+        console.log(error);
+      }
+    });
+}
+
+//sayfada modulün olup olmadigini, varsa kelime listesinin
+async function controller() {
+  return new Promise((resolve, reject) => {
+    if (neueAbfrage == "") reject("notWort");
+    if (typeof worteList === "undefined") {
+      const worteList = [];
+      window.worteList = worteList;
+    }
+    worteList = neueAbfrage.split(",");
+    const lastWortList = storage.get("lastWortList")
+      ? storage.get("lastWortList").value
+      : [];
+    resolve(
+      worteList.length === lastWortList.length &&
+        worteList.every((val, index) => val === lastWortList[index])
+    );
+  });
+}
+
+async function loadBase() {
+  return new Promise((resolve, reject) => {
+    baseFun();
+    let duration = 10;
+    setTimeout(() => {
+      if (typeof item === "object") resolve();
+      reject(
+        `baseFun modülü ${duration} ms icerisinde sayfaya import edilemedi!\nSüreyi artirarak dene! Hata devam etmesi halinde modul pathini check et.(m:importModul, f:loadBase)`
+      );
+    }, duration);
+  });
+}
+
+/*
+async function  _worteList  () {
+  getWorteList();
+  item.search("byController.wortList", item.typ.variabel, getHTMLdoc,maxTime);
+};
+*/
+
+async function getHTMLdoc() {
+  //delete byController.wortList
+  maxTime = worteList.length < 5 ? maxTime : 300 * worteList.length;
+  if (typeof HTMLdocs !== "undefined") HTMLdocs.length = 0; //doc sifirlanir
+  getDoc();
+  item.search(" byController.docs", item.typ.variabel, wortObj, maxTime);
+}
+
+async function wortObj() {
+  delete byController.docs;
+  getWortObject(runApp);
+  item.search("byController.worts", item.typ.variabel, get_langTR, maxTime);
+}
+
+//wortObjsArr dizininde tutulunan wortObj'ler icin lang_TR durumu kontrol edilir ve yoksa gapi ile Türkcesi alinir.
+async function get_langTR() {
+  delete byController.worts; //kontrol islemi sonrasi controlObj'deki worts property kaldirilir...
+  getLang(); //Türkce karsiligi...
+  item.search("byController.trLang", item.typ.variabel, get_Image, maxTime);
+}
+
+async function get_Image() {
+  delete byController.trLang; //kontrol islemi sonrasi controlObj'deki trLang property kaldirilir...
+  getImg(); //görseller alinir...
+  item.search("byController.image", item.typ.variabel, finish, maxTime);
+}
+
+async function finish() {
+  delete byController.image;
+  storage.set("lastWortList", worteList, 3);
+  console.clear();
+  msg.allPrint();
+  //sonuclar ekrana basilir...
+  wortObjsArr.forEach((w) => {
+    let result = new Promise((resolve) => {
+      msg.group(1, w.wrt.wort, " kelimesi icin alinan sonuclar:");
+      console.log(JSON.stringify(w));
+      console.dir(w);
+      resolve();
+    });
+    result.then(msg.group());
+  });
+}
+
+await loadBase()
+  .then(appRun())
+  .catch((err) => {
+    console.log(err, "m:getModuls, p:loadBase.then()");
+  });
+
+//_______________________-
+
+/* --- cdn dosyasini kontrol eder --- */
+/*
+async function checkFile(url, pos = "(m:appStarter, f:checkFile)") {
+  urlChecker.url = false; //obje degeri default hale getirilir...
+  await fetch(url)
+    .then((response) => {
+      if (response.status === 404) throw 404;
+      urlChecker.url = true;
+    })
+    .catch((err) => {
+      let txt_404 = [
+          "warn",
+          `Dosya konumu hatali! Url'yi kontrol edin. ${pos}\n${url}\n`,
+        ],
+        txt_e = ["error", `Hata meydana geldi! ${pos}\n`],
+        txt = err === 404 ? txt_404 : txt_e;
+      window.console[txt[0]](txt[1], err);
+    });
+}
+*/
+/*
+Dizin Yapisi:
+📂
+  |_📇appStarter.js         .../appStarter.js"
+  |_📇getMosuls_*.js        .../getMosuls_*.js"  
+  |_📂WortApp               .../wortApp
+    |_📇_importModuls_*.js  .../wortApp/_importModuls_*.js  📍
+    |_📂module                  ./module
+    |_📇_Documents_*.js         ./module/_documents_*.js 
+    |_📇_img_*.js               ./module/_img_*.js 
+    |_📇_lang_*.js              ./module/_lang_*.js
+    |_📇_wortList_*.js          ./module/_wortList_*.js   🟡
+    |_📇_wortObj_*.js           ./module/_wortObj_*.js
+    |_📇_zBase_*.js             ./module/_zBase_*.js      🟡
+    |_📇wortList.json           ./module/wortList.json    
+*/
