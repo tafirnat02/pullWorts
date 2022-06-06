99589d82836d0a347a9b55207ff0a87484a26691
tafirnat02 <103073188+tafirnat02@users.noreply.github.com>
2022-06-06 05:27:53 +0200

    fdfdgd

--- appStarter.js
+++ appStarter.js
@@ -2,26 +2,37 @@
 var neueAbfrage="";// ör:"Gemüse,Fenster,Tüte"
 
 //modul import js dosyasi
-const url_moduls = "https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/wortApp/_importModuls_n17.js"
-//modul dosya durumu kontrol edilir
+const modulFiles = "https://cdn.jsdelivr.net/gh/tafirnat02/pullWorts@main/wortApp/_importModuls_n18.js"
 
+try {
+  appRun()
+ } catch (error) {
+  if (existFile(modulFiles)) loadModuls.call()
+ }
+ 
  //sayfaya modulleri import eder
  function loadModuls(){return new Promise((resolve)=>{let head_ =
    document.querySelector("head"),scrpt = document.createElement("script"); 
-   scrpt.type = "module";scrpt.src = url_moduls;document.head.appendChild(scrpt);
+   scrpt.type = "module";scrpt.src = modulFiles;document.head.appendChild(scrpt);
  head_.insertBefore(scrpt, head_.lastChild);resolve()});}
 
 function existFile(url){
-if(typeof runApp !== 'undefined') runApp()
 var http = new XMLHttpRequest();
 http.open('HEAD', url, false);http.send();
  if(http.status===200) return true
      let txt_404 = ['warn',`Dosya konumu hatali! Url'yi kontrol edin.\n${url}\n`],
       txt_e=['error',`Hata meydana geldi!\n`],
       txt = http.status===404?txt_404:txt_e;
-      window.console[txt[0]](txt[1],err) 
+      window.console[txt[0]](txt[1],http) 
     return false
  }
 
- if (existFile(url_moduls)) loadModuls.call()
+
+
+ 
+
+
+
+
+ 
 
